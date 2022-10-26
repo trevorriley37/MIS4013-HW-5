8911d7c0cf1d9635678f6446cb4b637d6eeb0b8c
Trevor Riley <Trevor.D.Riley-1@ou.edu>
2022-10-26 13:54:32 -0500

    f

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 function toCelsius(f) {
   return (5/9) * (f-32);
 }
-document.getElementById("demo").innerHTML = toCelsius;
+document.getElementById("f").innerHTML = toCelsius;
 </script>
 
 </body>
