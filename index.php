ac13cf9209d2b4a1ab444cf1be6187910f44976f
Trevor Riley <Trevor.D.Riley-1@ou.edu>
2022-10-26 14:07:08 -0500

    idk

--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 <html>
 <body>
 
-<h2>JavaScript Functions</h2>
+<h1>JavaScript Functions</h1>
+
+<h3>Farenheit to Celsius </h3>
 
 <p>Accessing a function without () will return the function definition instead of the function result:</p>
 <p id="f"></p>
 
 <script>
 function toCelsius() {
-	var f = prompt("Please enter a number", "1");
+	var f = prompt("Please enter the tempature in Farenheit", "1");
 	document.getElementById("answer").innerHTML =  (5/9) * (f-32);
 }
 </script>
